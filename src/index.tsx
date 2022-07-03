b9cee86f8980be9b15d9bf988106db2065f98790
u-one <bacflip270bb@gmail.com>
2022-07-03 19:32:14 +0900

    react-router 導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
 import Game from './pages/Game'
+import Top from './pages/Top'
+
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Game />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Top />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/pages/Top.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+const Top = () => {
+  return (
+      <div>
+          <h1>Top</h1>
+          <nav>
+            <Link to="/Game">Game</Link>
+          </nav>
+      </div>
+  )
+}
+
+export default Top;

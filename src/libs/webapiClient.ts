07ca100815fc54cf4de95d443b01595758639dd6
u-one <backflip270bb@gmail.com>
2023-04-15 17:53:09 +0900

    update api url

--- src/libs/webapiClient.ts
+++ src/libs/webapiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const trainModelApi = axios.create(
     {
-        baseURL: "https://uone-train-model-manager-dev.herokuapp.com/api/trainmodel/",
+        baseURL: "https://trainmodel-manager.herokuapp.com/api/trainmodel/",
         responseType: "json",
         headers: {
             "Content-Type": "application/json",
